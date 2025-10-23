0d692f0d8d49c0925eda9a85235e27321cc05452
Wojciech Kuśmierski <wojtek@tujko.pl>
2025-10-23 12:36:01 +0200

    instrukcje warunkowe - ćwiczenia

---
+++ Instrukcje_warunkowe_zadania.py
@@ -0,0 +1,24 @@
+#Rozwiązywanie równania kwadratowego
+'''
+a = float(input('Podaj liczbę a =/= 0: '))
+b = float(input('Podaj liczbę b: '))
+c = float(input('Podaj liczbę c: '))
+
+delta = b ** 2 - 4 * a * c
+
+if delta > 0:
+    x1 = (-b - delta ** 0.5) / (2 * a)
+    x2 = (-b + delta ** 0.5) / (2 * a)
+    print(f'x1 = {x1} v x2 = {x2}')
+elif delta == 0:
+    x = (-b - delta ** 0.5) / (2 * a)
+    print('x1 = x2 = {}'.format(x))
+else:
+    print('Brak rozwiązań')
+'''
+#Zadanie 12.
+pisemny_j_polski = int(input('pisemny polski'))
+pisemny_j_polski = int(input('pisemny polski'))
+pisemny_j_polski = int(input('pisemny polski'))
+pisemny_j_polski = int(input('pisemny polski'))
+pisemny_j_polski = int(input('pisemny polski'))

0678d7c2475dd11d71e5f3d8aeff6154278b4f9e
Wojciech Kuśmierski <wojtek@tujko.pl>
2025-12-16 14:35:43 +0100

    petla while zadania

--- Pętla_while.py
+++ Pętla_while.py
@@ -1,4 +1,6 @@
 #Pętla while - przykłady
+import time
+import random
 
 liczba = 120
 licznik = 0
@@ -19,7 +21,7 @@ while x != 'q':
 print(licznik1)
 
 #Zadanie 2.
-poprawne_haslo = 'DuPa'
+poprawne_haslo = 'dupa'
 haslo = input('Podaj hasło')
 proba = 1
 
@@ -35,3 +37,49 @@ else:
     print('Nie ma hasła - nie ma dostępu')
 
 #Dom. zad. 2 i 3 z teams
+#(Jest na telefonie)
+
+#Zadanie 6.
+wynik1 = 0
+wynik2 = 0
+akcja = 0
+
+while not ((wynik1 >= 21 or wynik2 >= 21) and abs(wynik1 - wynik2) >= 2): #abs(x) = |x|
+    akcja += 1 #akcja = akcja + 1
+    print(f'Akcja {akcja}')
+    #druzyna = int(input('Podaj numer wygranej drużyny'))
+    druzyna = random.randint(1, 2,)
+    if druzyna == 1:
+        wynik1 += 1
+    else:
+        wynik2 += 1
+    print(f'Wynik {wynik1} : {wynik2}')
+    time.sleep(0.1)
+
+if wynik1 > wynik2:
+    print('Wygrała drużyna 1')
+else:
+    print('Wygrała drużyna 2')
+
+#Zadanie 7.
+liczba2 = int(input('Podaj liczbę'))
+
+while liczba > 0:
+    cyfra = liczba % 10
+    liczba2 = liczba2 // 10
+    print(cyfra, end = '')
+
+#Zadanie 8.
+liczba3 = int(input('Podaj liczbę'))
+d = 2
+ile_czyn = 0
+ile_r_czyn = 0
+while liczba > 1:
+    if liczba % d == 0:
+        ile_r_czyn += 1
+    while liczba % d == 0:
+        liczba3 = liczba3 // d
+        ile_czyn += 1
+    d += 1
+print(ile_czyn)
+print(ile_r_czyn)
